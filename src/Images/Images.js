1b0271316a6738dde333621e98c09a8543ea2151
Safarov <elkhan.safarov10@yandex.com>
2020-05-13 14:20:23 +0500

    Исправить коментарии

--- src/Images/Images.js
+++ src/Images/Images.js
@@ -6,28 +6,93 @@ import './Images.css';
 class Images extends React.Component {
   state = {
     loading: true,
-    show: true
+    show: true,
+    addComment: [{
+      id: '',
+      name: '',
+      text: '',
+      date: ''
+
+    }]
   }
   componentDidMount() {
     fetch('https://boiling-refuge-66454.herokuapp.com/images')
       .then((response) => response.json())
       .then((jsonStr) => this.props.addImages(jsonStr))
+
   }
   componentDidUpdate(nextProps) {
     if(nextProps.photos !== this.props.photos) {
       this.setState({loading: false})
     }
+    console.log(this.props.comments)
   }
   displayImages = (id) => {
     fetch('https://boiling-refuge-66454.herokuapp.com/images/'+ id)
       .then((response) => response.json())
       .then((jsonStr) => this.props.add_modal(jsonStr))
+
+    fetch('https://boiling-refuge-66454.herokuapp.com/images/'+ id)
+      .then((response) => response.json())
+      .then((jsonStr) => this.props.add_comment(jsonStr.comments))
     this.setState({show: true})
   }
   handleShow = () => {
     this.setState({show: false})
   }
-  render() { 
+  handlerComment = (id) => {
+    return event => {
+      event.preventDefault();
+      let elements = event.target.elements;
+      let name = elements['name'].value;
+      let text = elements['comment'].value;
+      // let idx;
+      // if(id === 237) {
+      //   idx = 153
+      // } if(id === 238) {
+      //   idx = 155
+      // } if(id === 239) {
+      //   idx = 156
+      // } if(id === 240) {
+      //   idx = 154
+      // } if(id === 241) {
+      //   idx = 157
+      // } if(id === 242) {
+      //   idx = 158
+      // }
+      this.setState({
+        addComment: {
+          id,
+          name,
+          text,
+          date: Date.now()
+        }
+      })
+      if(this.state.addComment.id !== undefined) {
+        let comment = {
+          id: this.state.addComment.id,
+          name: this.state.addComment.name,
+          text: this.state.addComment.text,
+          date: this.state.addComment.date
+        }
+        console.log(comment)
+        this.props.new_add_comment(comment)
+      }
+    }
+  }
+  // handlerName = (id) => {
+  //   return event => {
+  //     this.setState({
+  //       addComment: {id, name: event.target.value}
+  //     })
+  //   }
+  // }
+  // handlerName = (event) => {
+  //   this.setState({
+  //     addComment: {text: event.target.value, date: Date.now()}
+  //   })
+  // }
+  render() {
     return (
       <div className="gallery">
         {
@@ -57,16 +122,37 @@ class Images extends React.Component {
                     <div className="largeImage">
                       <img src={item.url} alt="" />
                     </div>
-                    <input className="name" type="text" placeholder="Ваше имя"/>
-                    <input className="comment" type="text" placeholder="Ваш комментарий"/>
-                    <button className="button" type="button">Оставить комментарий</button>
-                    <div className="comments">
+                    <form onSubmit={this.handlerComment(item.id)}>
+                      <input 
+                        className="name" 
+                        type="text" name="name" 
+                        placeholder="Ваше имя" />
+                      <input 
+                        className="comment" 
+                        type="text" name="comment" 
+                        placeholder="Ваш комментарий" />
+                      <button className="button">Оставить комментарий</button>
+                    </form>
+                    {/* <div className="comments">
                       <div className="comment_1">
                         <span>{item.comments[0]  !== undefined ?  ('0' + new Date(item.comments[0].date).getDate() + '.' + 
                         '0' + (new Date(item.comments[0].date).getMonth() + 1) + '.' +
                         new Date(item.comments[0].date).getFullYear()) : null}</span>
                         <p>{item.comments[0] !== undefined ? item.comments[0].text : null}</p>
                       </div>
+                    </div> */}
+                    <div className="comments">
+                    {
+                      this.props.comments.map((item) => {
+                        return(
+                                <div className="comment_1">
+                                  <span>{item.date  !== undefined ?  (new Date(item.date).getDate() + '.' + 
+                                  '0' + (new Date(item.date).getMonth() + 1) + '.' +
+                                  new Date(item.date).getFullYear()) : null}</span>
+                                  <p>{item.text !== undefined ? item.text : null}</p>
+                                </div>)
+                      })
+                    }
                     </div>
                 </div>
               </div>
@@ -82,7 +168,8 @@ class Images extends React.Component {
 let mapStateToProps = (state) => {
   return {
     photos: state.images.photos,
-    modalPhoto: state.images.modalPhoto
+    modalPhoto: state.images.modalPhoto,
+    comments: state.images.comments
   }
 }
 let mapDispatchToProps = (dispatch) => {
@@ -94,6 +181,14 @@ let mapDispatchToProps = (dispatch) => {
     add_modal: (image) => dispatch({
       type: 'MODAL_PHOTOS',
       image
+    }),
+    add_comment: (comments) => dispatch({
+      type: 'MODAL_COMMENT',
+      comments
+    }),
+    new_add_comment: (comment) => dispatch({
+      type: 'ADD_COMMENT',
+      comment
     })
   }
 }
--- src/store/reducers/images.js
+++ src/store/reducers/images.js
@@ -1,6 +1,7 @@
 let initialState = {
     photos: [],
-    modalPhoto: []
+    modalPhoto: [],
+    comments: []
 }
 const reducer = function(state = initialState, action) {
     switch(action.type) {
@@ -12,7 +13,7 @@ const reducer = function(state = initialState, action) {
                 photos
             }
             break;
-        case 'MODAL_PHOTOS':
+        case 'MODAL_PHOTOS': 
             let modalPhoto = [...state.modalPhoto];
             modalPhoto = [action.image]
             return {
@@ -20,9 +21,26 @@ const reducer = function(state = initialState, action) {
                 modalPhoto
             }
             break;
+        case 'MODAL_COMMENT':
+            let comments = [...state.comments];
+            comments = [action.comments]
+            return {
+                ...state,
+                comments
+            }
+            break;
+        case 'ADD_COMMENT':
+            let addComments = state.comments.push(action.comment)
+            // let addComments = [...state.comments];
+            // addComments = [action.comment];
+            // console.log('wgegwewgeewg', action.comment)
+            return {
+                ...state,
+                addComments
+            }
+            break;
         default:
             return state;
     }
 }
-
 export default reducer;
